477bdafc9681bd2ac8da9a7c31e5da6599899d8f
AoAlenaA <aoanuchina@edu.hse.ru>
2025-06-18 16:53:20 +0500

    Created All main files and added them into route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import CreateGame from './components/CreateGame';
+import JoinGame from './components/JoinGame';
 import Game from './components/Game';
 import Rules from './components/Rules';
 import NotFound from './components/NotFound';
@@ -8,15 +11,17 @@ function App() {
 
   return (
     <Router>
-      <div className>
-        
+      <div className="App">
+      
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/create-game" element={<CreateGame />} />
+          <Route path="/join-game" element={<JoinGame />} />
           <Route path="/game" element={<Game />} />
           <Route path="/rules" element={<Rules />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-    
+        
       </div>
     </Router>
   );
