16a1d6c8461fd0fad99ea83e338deca640777449
veronikaKharit <113134807+veronikaKharit@users.noreply.github.com>
2025-06-18 16:20:12 +0500

    base files for game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Game from './components/Game';
+import Rules from './components/Rules';
+import NotFound from './components/NotFound';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Gool luck!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className>
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/rules" element={<Rules />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+    
+      </div>
+    </Router>
   );
 }
 
